4bbe507a07a0d8c6dc8d79145ea6d661515f983a
azizkap21 <aziz@azizweb.com>
2020-12-15 15:04:21 +0000

    New solution to test mono repo

---
+++ src/SPT.eCommerce.Cart/SPT.eCommerce.Cart.Container/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SPT.eCommerce.Cart.Container.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { message = "Cart Container is running!" });
+        }
+    }
+}
